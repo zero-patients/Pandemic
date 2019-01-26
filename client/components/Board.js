eb6c30914ac1308e5db9a048df65818a6d133722
Frank Rose <franklin.rose@gmail.com>
2019-01-26 00:13:03 -0500

    Initial map

--- client/components/Board.js
+++ client/components/Board.js
@@ -1,4 +1,7 @@
+/* eslint-disable complexity */
+/* eslint-disable max-statements */
 import React from 'react'
+import worldMap from './map.js'
 // import WorldMap from '../../public/worldmap'
 
 // const Board = () => {
@@ -30,21 +33,145 @@ class Board extends React.Component {
   }
 
   componentDidMount() {
-    // console.log('Component mounted')
     const canvas = this.canvasRef.current
+    console.log(canvas.style)
+    console.log(worldMap)
+    canvas.style.backgroundColor = '#87cefa'
+    canvas.style.color = '#fffaf0'
+    const padding = 20
+    canvas.style.padding = padding
+    const sBorderColor = '#cdc9c9'
     const ctx = canvas.getContext('2d')
     const width = canvas.width
     const height = canvas.height
     // console.log(width, height)
-    ctx.save()
-    ctx.beginPath()
+    // ctx.save()
+    // ctx.beginPath()
+    // ctx.clearRect(0, 0, width, height)
+    // ctx.lineWidth = 1
+    // ctx.translate(width / 2, height / 2)
+    // ctx.rotate(0 * Math.PI / 180)
+    // ctx.fillStyle = '#4397AC'
+    // ctx.fillRect(10, 10, 100, 100)
+    // ctx.restore()
     ctx.clearRect(0, 0, width, height)
     ctx.lineWidth = 1
-    ctx.translate(width / 2, height / 2)
-    ctx.rotate(0 * Math.PI / 180)
-    ctx.fillStyle = '#4397AC'
-    ctx.fillRect(10, 10, 100, 100)
-    ctx.restore()
+    const sZoom = 'ca,cl,us,ru'
+    let iOffsetX = 0
+    let iOffsetY = 0
+    const aZoom = sZoom.split(',')
+    let iMinX = worldMap[aZoom[0]][0][0][0]
+    let iMaxX = worldMap[aZoom[0]][0][0][0]
+    let iMinY = worldMap[aZoom[0]][0][0][1]
+    let iMaxY = worldMap[aZoom[0]][0][0][1]
+
+    for (let iCountry = 0; iCountry < aZoom.length; iCountry++) {
+      for (let iPath = 0; iPath < worldMap[aZoom[iCountry]].length; iPath++) {
+        for (
+          let iCoord = 0;
+          iCoord < worldMap[aZoom[iCountry]][iPath].length;
+          iCoord++
+        ) {
+          iMinX = Math.min(iMinX, worldMap[aZoom[iCountry]][iPath][iCoord][0])
+          iMaxX = Math.max(iMaxX, worldMap[aZoom[iCountry]][iPath][iCoord][0])
+          iMinY = Math.min(iMinY, worldMap[aZoom[iCountry]][iPath][iCoord][1])
+          iMaxY = Math.max(iMaxY, worldMap[aZoom[iCountry]][iPath][iCoord][1])
+        }
+      }
+    }
+
+    const iRatio = Math.min(
+      (width - padding) / (iMaxX - iMinX),
+      (height - padding) / (iMaxY = iMinY)
+    )
+
+    let iMidX = iMinX + (iMaxX - iMinX) / 2
+    let iMidY = iMinY + (iMaxY - iMinY) / 2
+    iOffsetX = iMidX * iRatio - width / 2
+    iOffsetY = iMidY * iRatio - height / 2
+
+    const Draw = (sCountry, sColor) => {
+      const oCTX = ctx
+      oCTX.fillStyle = sColor
+      oCTX.strokeStyle = sBorderColor
+      oCTX.beginPath()
+
+      // loop through paths
+      let bIE = navigator.userAgent.indexOf('MSIE') > -1
+      for (let iPath = 0; iPath < worldMap[sCountry].length; iPath++) {
+        oCTX.moveTo(
+          worldMap[sCountry][iPath][0][0] * iRatio - iOffsetX,
+          worldMap[sCountry][iPath][0][1] * iRatio - iOffsetY
+        )
+        for (
+          let iCoord = 1;
+          iCoord < worldMap[sCountry][iPath].length;
+          iCoord++
+        ) {
+          oCTX.lineTo(
+            worldMap[sCountry][iPath][iCoord][0] * iRatio - iOffsetX,
+            worldMap[sCountry][iPath][iCoord][1] * iRatio - iOffsetY
+          )
+        }
+        oCTX.closePath()
+        oCTX.fill()
+
+        // IE, again...
+        if (bIE === true) {
+          oCTX.beginPath()
+          oCTX.moveTo(
+            worldMap[sCountry][iPath][0][0] * iRatio - iOffsetX,
+            worldMap[sCountry][iPath][0][1] * iRatio - iOffsetY
+          )
+          for (
+            let iCoord = 1;
+            iCoord < worldMap[sCountry][iPath].length;
+            iCoord++
+          ) {
+            oCTX.lineTo(
+              worldMap[sCountry][iPath][iCoord][0] * iRatio - iOffsetX,
+              worldMap[sCountry][iPath][iCoord][1] * iRatio - iOffsetY
+            )
+          }
+          oCTX.closePath()
+        }
+        oCTX.stroke()
+      }
+
+      // awful hack for Lesotho / South Africa (draw Lesotho again, kids!)
+      if (sCountry === 'za') {
+        // choose colour
+        // if (oSettings.detail.ls) {
+        //   oCTX.fillStyle = oSettings.detail.ls
+        // } else {
+        //   oCTX.fillStyle = sFGColor
+        // }
+
+        // loop through paths
+        oCTX.beginPath()
+        for (var iPath = 0; iPath < worldMap.ls.length; iPath++) {
+          oCTX.moveTo(
+            worldMap.ls[iPath][0][0] * iRatio - iOffsetX,
+            worldMap.ls[iPath][0][1] * iRatio - iOffsetY
+          )
+          for (let iCoord = 1; iCoord < worldMap.ls[iPath].length; iCoord++) {
+            oCTX.lineTo(
+              worldMap.ls[iPath][iCoord][0] * iRatio - iOffsetX,
+              worldMap.ls[iPath][iCoord][1] * iRatio - iOffsetY
+            )
+          }
+          oCTX.closePath()
+          oCTX.fill()
+          oCTX.stroke()
+        }
+      }
+    }
+
+    for (let country in worldMap) {
+      if (worldMap.hasOwnProperty(country)) {
+        Draw(country, canvas.style.color)
+      }
+    }
   }
 
   render() {
--- client/components/map.js
+++ client/components/map.js
@@ -1,179 +1,4 @@
-/* eslint-disable eqeqeq */
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-undef */
-/* eslint-disable complexity */
-/* eslint-disable max-statements */
-/*
- * Canvas World Map function (documentation: http://joncom.be/code/excanvas-world-map)
- *
- * Copyright (c) 2009 Jon Combe (http://joncom.be)
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
-
-function WorldMap(oSettings) {
-  // create variables
-  var oSettings = oSettings || {}
-  oSettings.detail = oSettings.detail || {}
-  var sBGColor = oSettings.bgcolor || '#87cefa'
-  var sFGColor = oSettings.fgcolor || '#fffaf0'
-  var sBorderColor = oSettings.bordercolor || '#cdc9c9'
-  var iPadding = (oSettings.padding || 10) * 2
-  var sZoom = oSettings.zoom || 'ca,cl,us,ru'
-  var iOffsetX = 0
-  var iOffsetY = 0
-
-  // get canvas dimensions, set bgcolor
-  var oCanvas = document.getElementById(oSettings.id)
-  if (!oCanvas) {
-    alert("Error: missing or incorrect canvas 'id'")
-  }
-  var iCanvasWidth = oCanvas.width
-  var iCanvasHeight = oCanvas.height
-  oCanvas.style.backgroundColor = sBGColor
-
-  // create drawing area
-  var oCTX = oCanvas.getContext('2d')
-  oCTX.clearRect(0, 0, iCanvasWidth, iCanvasHeight)
-  oCTX.lineWidth = oSettings.borderwidth || 1
-
-  // calculate zoom: create variables
-  var aZoom = sZoom.split(',')
-  var iMinX = oWorldMap[aZoom[0]][0][0][0]
-  var iMaxX = oWorldMap[aZoom[0]][0][0][0]
-  var iMinY = oWorldMap[aZoom[0]][0][0][1]
-  var iMaxY = oWorldMap[aZoom[0]][0][0][1]
-
-  // calculate zoom: find map range
-  for (var iCountry = 0; iCountry < aZoom.length; iCountry++) {
-    for (var iPath = 0; iPath < oWorldMap[aZoom[iCountry]].length; iPath++) {
-      for (
-        iCoord = 0;
-        iCoord < oWorldMap[aZoom[iCountry]][iPath].length;
-        iCoord++
-      ) {
-        iMinX = Math.min(iMinX, oWorldMap[aZoom[iCountry]][iPath][iCoord][0])
-        iMaxX = Math.max(iMaxX, oWorldMap[aZoom[iCountry]][iPath][iCoord][0])
-        iMinY = Math.min(iMinY, oWorldMap[aZoom[iCountry]][iPath][iCoord][1])
-        iMaxY = Math.max(iMaxY, oWorldMap[aZoom[iCountry]][iPath][iCoord][1])
-      }
-    }
-  }
-
-  // calculate zoom ratio
-  var iRatio = Math.min(
-    (iCanvasWidth - iPadding) / (iMaxX - iMinX),
-    (iCanvasHeight - iPadding) / (iMaxY - iMinY)
-  )
-
-  // calculate zoom offsets
-  var iMidX = iMinX + (iMaxX - iMinX) / 2
-  var iMidY = iMinY + (iMaxY - iMinY) / 2
-  iOffsetX = iMidX * iRatio - iCanvasWidth / 2
-  iOffsetY = iMidY * iRatio - iCanvasHeight / 2
-
-  // draw "plain" countries
-  for (var sCountry in oWorldMap) {
-    Draw(sCountry, sFGColor)
-  }
-
-  // draw "details" countries
-  for (var sCountry in oSettings.detail) {
-    if (oWorldMap[sCountry]) {
-      Draw(sCountry, oSettings.detail[sCountry])
-    }
-  }
-
-  // private draw function
-  function Draw(sCountry, sColor) {
-    oCTX.fillStyle = sColor
-    oCTX.strokeStyle = sBorderColor
-    oCTX.beginPath()
-
-    // loop through paths
-    var bIE = navigator.userAgent.indexOf('MSIE') > -1
-    for (var iPath = 0; iPath < oWorldMap[sCountry].length; iPath++) {
-      oCTX.moveTo(
-        oWorldMap[sCountry][iPath][0][0] * iRatio - iOffsetX,
-        oWorldMap[sCountry][iPath][0][1] * iRatio - iOffsetY
-      )
-      for (iCoord = 1; iCoord < oWorldMap[sCountry][iPath].length; iCoord++) {
-        oCTX.lineTo(
-          oWorldMap[sCountry][iPath][iCoord][0] * iRatio - iOffsetX,
-          oWorldMap[sCountry][iPath][iCoord][1] * iRatio - iOffsetY
-        )
-      }
-      oCTX.closePath()
-      oCTX.fill()
-
-      // IE, again...
-      if (bIE == true) {
-        oCTX.beginPath()
-        oCTX.moveTo(
-          oWorldMap[sCountry][iPath][0][0] * iRatio - iOffsetX,
-          oWorldMap[sCountry][iPath][0][1] * iRatio - iOffsetY
-        )
-        for (iCoord = 1; iCoord < oWorldMap[sCountry][iPath].length; iCoord++) {
-          oCTX.lineTo(
-            oWorldMap[sCountry][iPath][iCoord][0] * iRatio - iOffsetX,
-            oWorldMap[sCountry][iPath][iCoord][1] * iRatio - iOffsetY
-          )
-        }
-        oCTX.closePath()
-      }
-      oCTX.stroke()
-    }
-
-    // awful hack for Lesotho / South Africa (draw Lesotho again, kids!)
-    if (sCountry == 'za') {
-      // choose colour
-      if (oSettings.detail.ls) {
-        oCTX.fillStyle = oSettings.detail.ls
-      } else {
-        oCTX.fillStyle = sFGColor
-      }
-
-      // loop through paths
-      oCTX.beginPath()
-      for (var iPath = 0; iPath < oWorldMap.ls.length; iPath++) {
-        oCTX.moveTo(
-          oWorldMap.ls[iPath][0][0] * iRatio - iOffsetX,
-          oWorldMap.ls[iPath][0][1] * iRatio - iOffsetY
-        )
-        for (iCoord = 1; iCoord < oWorldMap.ls[iPath].length; iCoord++) {
-          oCTX.lineTo(
-            oWorldMap.ls[iPath][iCoord][0] * iRatio - iOffsetX,
-            oWorldMap.ls[iPath][iCoord][1] * iRatio - iOffsetY
-          )
-        }
-        oCTX.closePath()
-        oCTX.fill()
-        oCTX.stroke()
-      }
-    }
-  }
-}
-
-var oWorldMap = {
+const worldMap = {
   ad: [[[4687, 2398], [4679, 2402], [4679, 2398]]],
   ae: [
     [
@@ -1610,48 +1435,48 @@ var oWorldMap = {
       [4971, 1954]
     ]
   ],
-  dk: [
-    [
-      [4867, 1982],
-      [4891, 1982],
-      [4895, 1958],
-      [4915, 1978],
-      [4923, 1962],
-      [4907, 1954],
-      [4899, 1958],
-      [4899, 1954],
-      [4927, 1922],
-      [4911, 1918],
-      [4915, 1874],
-      [4887, 1898],
-      [4867, 1898],
-      [4851, 1922],
-      [4851, 1958],
-      [4867, 1962]
-    ],
-    [
-      [4967, 1938],
-      [4951, 1942],
-      [4951, 1950],
-      [4943, 1942],
-      [4931, 1954],
-      [4935, 1970],
-      [4959, 1978],
-      [4959, 1970],
-      [4967, 1966],
-      [4959, 1958],
-      [4971, 1954]
-    ],
-    [
-      [4931, 1978],
-      [4927, 1986],
-      [4943, 1994],
-      [4955, 1990],
-      [4963, 1978],
-      [4947, 1978],
-      [4943, 1982]
-    ]
-  ],
+  // dk: [
+  //   [
+  //     [4867, 1982],
+  //     [4891, 1982],
+  //     [4895, 1958],
+  //     [4915, 1978],
+  //     [4923, 1962],
+  //     [4907, 1954],
+  //     [4899, 1958],
+  //     [4899, 1954],
+  //     [4927, 1922],
+  //     [4911, 1918],
+  //     [4915, 1874],
+  //     [4887, 1898],
+  //     [4867, 1898],
+  //     [4851, 1922],
+  //     [4851, 1958],
+  //     [4867, 1962]
+  //   ],
+  //   [
+  //     [4967, 1938],
+  //     [4951, 1942],
+  //     [4951, 1950],
+  //     [4943, 1942],
+  //     [4931, 1954],
+  //     [4935, 1970],
+  //     [4959, 1978],
+  //     [4959, 1970],
+  //     [4967, 1966],
+  //     [4959, 1958],
+  //     [4971, 1954]
+  //   ],
+  //   [
+  //     [4931, 1978],
+  //     [4927, 1986],
+  //     [4943, 1994],
+  //     [4955, 1990],
+  //     [4963, 1978],
+  //     [4947, 1978],
+  //     [4943, 1982]
+  //   ]
+  // ],
   dm: [
     [
       [3024.5, 3173.5],
@@ -5295,3 +5120,5 @@ var oWorldMap = {
     ]
   ]
 }
+
+export default worldMap
