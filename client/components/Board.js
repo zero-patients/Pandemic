985e88f6f3a245e72138ff80f5c307fcb5e0d93a
Willy Mello <cwmello165@gmail.com>
2019-01-28 13:10:44 -0500

    stars added at city locations

--- client/components/Board.js
+++ client/components/Board.js
@@ -4,6 +4,7 @@ import React from 'react'
 import worldMap from './map.js'
 import mapDetails from './mapDetails'
 import db from '../../server/db'
+import cities from '../data/cities'
 
 class Board extends React.Component {
   constructor(props) {
@@ -190,6 +191,38 @@ class Board extends React.Component {
         oCTX.stroke()
       })
     })
+    function drawStar(cx, cy, spikes, outerRadius, innerRadius) {
+      var rot = Math.PI / 2 * 3
+      var x = cx
+      var y = cy
+      var step = Math.PI / spikes
+
+      ctx.beginPath()
+      ctx.moveTo(cx, cy - outerRadius)
+      for (let i = 0; i < spikes; i++) {
+        x = cx + Math.cos(rot) * outerRadius
+        y = cy + Math.sin(rot) * outerRadius
+        ctx.lineTo(x, y)
+        rot += step
+
+        x = cx + Math.cos(rot) * innerRadius
+        y = cy + Math.sin(rot) * innerRadius
+        ctx.lineTo(x, y)
+        rot += step
+      }
+      ctx.lineTo(cx, cy - outerRadius)
+      ctx.closePath()
+      ctx.lineWidth = 5
+      ctx.strokeStyle = 'blue'
+      ctx.stroke()
+      ctx.fillStyle = 'skyblue'
+      ctx.fill()
+    }
+    console.log(cities)
+    for (let place in cities) {
+      console.log(cities[place].location[0], 'place')
+      drawStar(cities[place].location[0], cities[place].location[1], 5, 30, 15)
+    }
   }
 
   render() {
