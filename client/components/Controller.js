5626bf78c75a7d347e1b060009003e46b113a101
Frank Rose <franklin.rose@gmail.com>
2019-01-27 21:13:40 -0500

    Controller updates Firestore

--- client/components/Controller.js
+++ client/components/Controller.js
@@ -1,15 +1,46 @@
 import React from 'react'
+import db from '../../server/db'
 
 const Controller = props => {
   const {userId} = props.match.params
   const style = `player${userId}`
+  const game = db.collection('rooms').doc('YzQ0qR6LZ7gxd8E03k1l')
+
+  const goToEurope = async () => {
+    const doc = await game.get()
+    const data = doc.data()
+    game.set({[`${style}Info`]: {location: {X: 910, Y: 410}}}, {merge: true})
+  }
+  const goToAmerica = async () => {
+    const doc = await game.get()
+    const data = doc.data()
+    game.set({[`${style}Info`]: {location: {X: 475, Y: 475}}}, {merge: true})
+  }
+  const goToSouthAmerica = async () => {
+    const doc = await game.get()
+    const data = doc.data()
+    game.set({[`${style}Info`]: {location: {X: 650, Y: 750}}}, {merge: true})
+  }
+  const goToAsia = async () => {
+    const doc = await game.get()
+    const data = doc.data()
+    game.set({[`${style}Info`]: {location: {X: 1500, Y: 515}}}, {merge: true})
+  }
 
   return (
     <div className={style}>
-      <button type="button">London</button>
-      <button type="button">Mexico City</button>
-      <button type="button">Riyadh</button>
-      <button type="button">Seoul</button>
+      <button type="button" onClick={() => goToEurope()}>
+        Europe
+      </button>
+      <button type="button" onClick={() => goToAmerica()}>
+        America
+      </button>
+      <button type="button" onClick={() => goToSouthAmerica()}>
+        South America
+      </button>
+      <button type="button" onClick={() => goToAsia()}>
+        Asia
+      </button>
     </div>
   )
 }
