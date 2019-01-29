85e188937bc2c23af5cddb72cd2db55c4d3ea0ce
Ryan Kerins <ryanakerins@gmail.com>
2019-01-29 15:01:53 -0500

    updated shuffle funcs to not delete connections

---
+++ client/components/InfectionCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const InfectionCard = props => {
+  return (
+    <div id="infectionCard">
+      <h1>{props.cityName}</h1>
+      {/* <img></img> */}
+    </div>
+  )
+}
