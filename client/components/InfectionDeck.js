5d34b0af9946cb27ee38102d5bf5c0d4630b9bf2
Frank Rose <franklin.rose@gmail.com>
2019-01-30 16:35:57 -0500

    Deck skeleton files commited

---
+++ client/components/InfectionDeck.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class InfectionDeck extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Deck</p>
+      </div>
+    )
+  }
+}
+
+export default InfectionDeck
---
+++ client/components/InfectionDiscardDeck.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class InfectionDiscardDeck extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Deck</p>
+      </div>
+    )
+  }
+}
+
+export default InfectionDiscardDeck
