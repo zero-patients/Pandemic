c982ade50bae633c834811b573e5906574b97a36
Frank Rose <franklin.rose@gmail.com>
2019-01-31 15:15:12 -0500

    feature: Adding roles to data folder

---
+++ client/data/playerCards.js
@@ -0,0 +1,3 @@
+const playerCards = []
+
+module.exports = playerCards
---
+++ client/data/roles.js
@@ -0,0 +1,7 @@
+const roles = {
+  dispatcher: `- Move another player's pawn as if it were your own
+  - As an action, move any pawn to a city with another pawn`,
+  medic: `- Remove all cubes of one color when doing Treat Disease`
+}
+
+module.exports = roles
