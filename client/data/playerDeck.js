0defaabbc3990d8b0b663df348a093b4d68213f5
Ryan Kerins <ryanakerins@gmail.com>
2019-02-01 12:58:49 -0500

    Fix: Made player deck array and updated shuffle, loss functions

--- client/data/playerDeck.js
+++ client/data/playerDeck.js
@@ -1,274 +1,340 @@
-const playerDeck = {
-  Algiers: {
+const playerDeck = [
+  {
+    name: 'Algiers',
     type: 'city',
     color: 'black',
     population: 2946000
   },
-  Atlanta: {
+  {
+    name: 'Atlanta',
     type: 'city',
     color: 'blue',
     population: 4715000
   },
-  Baghdad: {
+  {
+    name: 'Baghdad',
     type: 'city',
     color: 'black',
     population: 6204000
   },
-  Bangkok: {
+  {
+    name: 'Bangkok',
     type: 'city',
     color: 'red',
     population: 7151000
   },
-  Beijing: {
+  {
+    name: 'Beijing',
     type: 'city',
     color: 'red',
     population: 17311000
   },
-  Bogota: {
+  {
+    name: 'Bogota',
     type: 'city',
     color: 'yellow',
     population: 8702000
   },
-  'Buenos Aires': {
+  {
+    name: 'Buenos Aires',
     type: 'city',
     color: 'yellow',
     population: 13639000
   },
-  Cairo: {
+  {
+    name: 'Cairo',
     type: 'city',
     color: 'black',
     population: 14718000
   },
-  Chennai: {
+  {
+    name: 'Chennai',
     type: 'city',
     color: 'black',
     population: 8865000
   },
-  Chicago: {
+  {
+    name: 'Chicago',
     type: 'city',
     color: 'blue',
     population: 9121000
   },
-  Delhi: {
+  {
+    name: 'Delhi',
     type: 'city',
     color: 'black',
     population: 22242000
   },
-  Essen: {
+  {
+    name: 'Essen',
     type: 'city',
     color: 'blue',
     population: 575000
   },
-  'Ho Chi Minh City': {
+  {
+    name: 'Ho Chi Minh City',
     type: 'city',
     color: 'red',
     population: 8314000
   },
-  'Hong Kong': {
+  {
+    name: 'Hong Kong',
     type: 'city',
     color: 'red',
     population: 7106000
   },
-  Istanbul: {
+  {
+    name: 'Istanbul',
     type: 'city',
     color: 'black',
     population: 13576000
   },
-  Jakarta: {
+  {
+    name: 'Jakarta',
     type: 'city',
     color: 'red',
     population: 26063000
   },
-  Johannesburg: {
+  {
+    name: 'Johannesburg',
     type: 'city',
     color: 'yellow',
     population: 3888000
   },
-  Karachi: {
+  {
+    name: 'Karachi',
     type: 'city',
     color: 'black',
     population: 20711000
   },
-  Khartoum: {
+  {
+    name: 'Khartoum',
     type: 'city',
     color: 'yellow',
     population: 4887000
   },
-  Kinshasa: {
+  {
+    name: 'Kinshasa',
     type: 'city',
     color: 'yellow',
     population: 9046000
   },
-  Kolkata: {
+  {
+    name: 'Kolkata',
     type: 'city',
     color: 'black',
     population: 14374000
   },
-  Lagos: {
+  {
+    name: 'Lagos',
     type: 'city',
     color: 'yellow',
     population: 11547000
   },
-  Lima: {
+  {
+    name: 'Lima',
     type: 'city',
     color: 'yellow',
     population: 9121000
   },
-  London: {
+  {
+    name: 'London',
     type: 'city',
     color: 'blue',
     population: 8586000
   },
-  'Los Angeles': {
+  {
+    name: 'Los Angeles',
     type: 'city',
     color: 'yellow',
     population: 14900000
   },
-  Madrid: {
+  {
+    name: 'Madrid',
     type: 'city',
     color: 'blue',
     population: 5427000
   },
-  Manila: {
+  {
+    name: 'Manila',
     type: 'city',
     color: 'red',
     population: 20767000
   },
-  'Mexico City': {
+  {
+    name: 'Mexico City',
     type: 'city',
     color: 'yellow',
     population: 19463000
   },
-  Miami: {
+  {
+    name: 'Miami',
     type: 'city',
     color: 'yellow',
     population: 5582000
   },
-  Milan: {
+  {
+    name: 'Milan',
     type: 'city',
     color: 'blue',
     population: 5232000
   },
-  Montreal: {
+  {
+    name: 'Montreal',
     type: 'city',
     color: 'blue',
     population: 3429000
   },
-  Moscow: {
+  {
+    name: 'Moscow',
     type: 'city',
     color: 'black',
     population: 15512000
   },
-  Mumbai: {
+  {
+    name: 'Mumbai',
     type: 'city',
     color: 'black',
     population: 16910000
   },
-  'New York': {
+  {
+    name: 'New York',
     type: 'city',
     color: 'blue',
     population: 20464000
   },
-  Osaka: {
+  {
+    name: 'Osaka',
     type: 'city',
     color: 'red',
     population: 2871000
   },
-  Paris: {
+  {
+    name: 'Paris',
     type: 'city',
     color: 'blue',
     population: 10755000
   },
-  Riyadh: {
+  {
+    name: 'Riyadh',
     type: 'city',
     color: 'black',
     population: 5037000
   },
-  'San Francisco': {
+  {
+    name: 'San Francisco',
     type: 'city',
     color: 'blue',
     population: 5864000
   },
-  Santiago: {
+  {
+    name: 'Santiago',
     type: 'city',
     color: 'yellow',
     population: 6015000
   },
-  'Sao Paulo': {
+  {
+    name: 'Sao Paulo',
     type: 'city',
     color: 'yellow',
     population: 20186000
   },
-  Seoul: {
+  {
+    name: 'Seoul',
     type: 'city',
     color: 'red',
     population: 22547000
   },
-  Shanghai: {
+  {
+    name: 'Shanghai',
     type: 'city',
     color: 'red',
     population: 13482000
   },
-  'St Petersburg': {
+  {
+    name: 'St Petersburg',
     type: 'city',
     color: 'blue',
     population: 4879000
   },
-  Sydney: {
+  {
+    name: 'Sydney',
     type: 'city',
     color: 'red',
     population: 3785000
   },
-  Taipei: {
+  {
+    name: 'Taipei',
     type: 'city',
     color: 'red',
     population: 8338000
   },
-  Tehran: {
+  {
+    name: 'Tehran',
     type: 'city',
     color: 'black',
     population: 7419000
   },
-  Tokyo: {
+  {
+    name: 'Tokyo',
     type: 'city',
     color: 'red',
     population: 13189000
   },
-  Washington: {
+  {
+    name: 'Washington',
     type: 'city',
     color: 'blue',
     population: 4679000
   },
-  'One Quiet Night': {
+  {
+    name: 'One Quiet Night',
     type: 'event',
     description:
       'Play at any time. Not an action. Skip the next infect cities step (Do not flip over any infection cards).'
   },
-  'Resilient Population': {
+  {
+    name: 'Resilient Population',
     type: 'event',
     description:
       'Play at any time. Not an action. Remove any 1 card in the infection discard pile from the game. You may play this between the infect and intensify steps of an epidemic.'
   },
-  Forecast: {
+  {
+    name: 'Forecast',
     type: 'event',
     description:
       'Play at any time. Not an action. Draw, look at, and rearrange the top 6 cards of the infection deck. Put them back on top'
   },
-  'Government Grant': {
+  {
+    name: 'Government Grant',
     type: 'event',
     description:
       'Play at any time. Not an action. Add 1 research station to any city (No city card needed).'
   },
-  Airlift: {
+  {
+    name: 'Airlift',
     type: 'event',
     description:
       "Play at any time. Not an action. Move any 1 pawn to any city. Get permission beore moving another player's pawn."
   },
-  Epidemic: {
+  {
+    name: 'Epidemic',
+    type: 'epidemic',
+    description:
+      '1-INCREASE\nMove the infection rate marker forward 1 space.\n2-INFECT\nDraw the bottom card from the infection deck and put 3 infection cubes on that city. Discard that card.\n3-INTENSIFY\nShuffle the cards in the infection discard pile and put them on top of the infection discard deck.'
+  },
+  {
+    name: 'Epidemic',
+    type: 'epidemic',
+    description:
+      '1-INCREASE\nMove the infection rate marker forward 1 space.\n2-INFECT\nDraw the bottom card from the infection deck and put 3 infection cubes on that city. Discard that card.\n3-INTENSIFY\nShuffle the cards in the infection discard pile and put them on top of the infection discard deck.'
+  },
+  {
+    name: 'Epidemic',
     type: 'epidemic',
     description:
       '1-INCREASE\nMove the infection rate marker forward 1 space.\n2-INFECT\nDraw the bottom card from the infection deck and put 3 infection cubes on that city. Discard that card.\n3-INTENSIFY\nShuffle the cards in the infection discard pile and put them on top of the infection discard deck.'
   }
-}
+]
 
 module.exports = playerDeck
--- client/funcs/isWinLoss.js
+++ client/funcs/isWinLoss.js
@@ -2,16 +2,42 @@ const db = require('../../server/db')
 
 const checkInfection = async () => {
   try {
-    let diseases = []
     await db
       .collection('rooms')
       .doc('YzQ0qR6LZ7gxd8E03k1l')
       .onSnapshot(doc => {
         const data = doc.data().infectionCounter
-        data.some(elem => elem > 24)
+        let dead = data.some(elem => elem > 24)
       })
   } catch (error) {
     console.error(error)
   }
 }
-checkInfection()
+
+const checkOutbreakCount = async () => {
+  try {
+    await db
+      .collection('rooms')
+      .doc('YzQ0qR6LZ7gxd8E03k1l')
+      .onSnapshot(async doc => {
+        const data = await doc.data().outbreakTracker
+        const dead = data >= 8
+      })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const checkPlayerDeckCount = async () => {
+  try {
+    await db
+      .collection('rooms')
+      .doc('YzQ0qR6LZ7gxd8E03k1l')
+      .onSnapshot(async doc => {
+        const data = await doc.data().playerDeck
+        const dead = data <= 0
+      })
+  } catch (err) {
+    console.error(err)
+  }
+}
