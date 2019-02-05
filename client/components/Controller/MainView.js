29cab976d1271f1a9062edfc0a791379d170bf75
Willy Mello <cwmello165@gmail.com>
2019-02-05 11:38:29 -0500

    feature: added cdc and airplane movement components

--- client/components/Controller/MainView.js
+++ client/components/Controller/MainView.js
@@ -144,6 +144,7 @@ class MainView extends Component {
       let playerInfo = data[`${this.playerId}Info`]
       console.log(playerInfo, 'playerInfo')
       let playerHand = playerInfo.hand
+      console.log(playerHand, 'playerHand')
       let playerCity = playerInfo.location
       let playerCityInfo = data.cities[playerCity]
 
